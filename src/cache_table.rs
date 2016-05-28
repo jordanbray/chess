f3e62703006e20bf383516c5f922558c2d5c1473
Jordan Bray <jordanbray@gmail.com>
2016-05-28 14:26:23 -0400

    Updated documentation.

--- src/cache_table.rs
+++ src/cache_table.rs
@@ -4,12 +4,17 @@ struct CacheTableEntry<T: Copy + Clone + PartialEq + PartialOrd > {
     entry: T
 }
 
+/// Store a cache of entries, each with an associated hash.
 pub struct CacheTable<T: Copy + Clone + PartialEq + PartialOrd> {
     table: Vec<CacheTableEntry<T>>,
     mask: usize
 }
 
 impl <T: Copy + Clone + PartialEq + PartialOrd> CacheTable<T> {
+    /// Create a new `CacheTable` with each associated entry initialized with a hash of '0'
+    /// Note: Y ou must pass in a size where only 1 bit is set. (AKA: 2, 4, 8, 16, 1024, 65536,
+    /// etc.)
+    /// Panics when size is invalid.
     pub fn new(size: usize, default: T) -> CacheTable<T> {
         if size.count_ones() != 1 {
             panic!("You cannot create a CacheTable with a non-binary number.");
@@ -22,6 +27,7 @@ impl <T: Copy + Clone + PartialEq + PartialOrd> CacheTable<T> {
         res
     }
 
+    /// Get a particular entry with the hash specified
     pub fn get(&self, hash: u64) -> Option<T> {
         let entry = unsafe { *self.table.get_unchecked((hash as usize) & self.mask) };
         if entry.hash == hash {
@@ -31,6 +37,7 @@ impl <T: Copy + Clone + PartialEq + PartialOrd> CacheTable<T> {
         }
     }
 
+    /// Add (or overwrite) an entry with the associated hash
     pub fn add(&mut self, hash: u64, entry: T) {
         let mut e = unsafe { self.table.get_unchecked_mut((hash as usize) & self.mask) };
         *e = CacheTableEntry { hash: hash, entry: entry };
