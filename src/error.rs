12dffd885d3a091662ece5cc0a56b466668aec4e
Jordan Bray <jordanbray@gmail.com>
2019-05-27 14:59:51 -0400

    Adds documentation to .

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,13 @@
 use failure::Fail;
 
+/// Sometimes, bad stuff happens.
 #[derive(Debug, Fail)]
 pub enum Error {
+    /// The FEN string is invalid
     #[fail(display = "Invalid FEN string: {}", fen)]
     InvalidFen { fen: String },
+
+    /// The board created from BoardBuilder was found to be invalid
     #[fail(
         display = "The board specified did not pass sanity checks.  Are you sure the kings exist and the side to move cannot capture the opposing king?"
     )]
